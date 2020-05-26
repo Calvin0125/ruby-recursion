a690cc2c17eeda9c70200b570017bb1f0464cd02
Calvin Conley <cconley0125@gmail.com>
2020-05-26 19:52:53 -0400

    finished recursive fibonacci functions

---
+++ fibonacci.rb
@@ -0,0 +1,25 @@
+def fib(n)
+    result = []
+    if n == 1
+        return result.push(0)
+    elsif n == 2
+        return result.push(0,1)
+    else
+        result.push(0,1)
+        accumulator = 2
+        (n-2).times do
+            result[accumulator] = result[accumulator-1] + result[accumulator-2]
+            accumulator += 1
+        end
+        return result
+    end
+end
+
+
+
+def fib_rec(n)
+    return [0] if n == 1
+    return [0,1] if n == 2
+    sequence = fib(n-1)
+    sequence.push(sequence[-1] + sequence[-2])
+end
